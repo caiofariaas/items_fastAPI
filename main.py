894dbdbcabd7e3e38982fc74598b968a14aea939
caiofariaas <caiofarias077@gmail.com>
2024-01-02 11:59:14 -0300

    crud completo🎉🎉

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from database import *
 from models import *
-
+from typing import List
 
 app = FastAPI()
 
@@ -33,11 +33,62 @@ async def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     
     return db_item
 
+# UPDATE para os items
+
+@app.put('/update_item/{item_id}', response_model=ItemRead)
+async def update_item(item_id: int, upd_item: ItemUpdate, db: Session = Depends(get_db)):
+    
+    db_item = db.query(ItemDB).filter(ItemDB.id == item_id).first()
+
+    if not db_item:
+        raise HTTPException(status_code=404, detail='Item Not Found')
+
+    # Criamos um for para percorrer todos os itens 'key : value'
+    # usamos '.dict()' para transformar o objeto pydantic em um dict
+    # items() retorna uma sequencia de tuplas onde cada uma contem um par key-value
+    
+    for key, value in upd_item.dict().items():
+        
+        # 'set attribute' é usada para definir o valor de um atributo em um objeto
+        # recebe como parametro o objeto, o nome do atributo que deseja definir ou modificar e o valor que deseja atribuir ao mesmo
+        
+        setattr(db_item, key, value)
+
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+# Função para excluir do banco de dados
+
+@app.delete('/delete_item/{item_id}')
+async def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(ItemDB).filter(ItemDB.id == item_id).first()
+    
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+        
+        return {'message': f'Item with id "{item_id}" deleted', 'Deleted_item': db_item}
+    else:
+        raise HTTPException(status_code=404, detail='Item Not Found')
+
+# GET para itens especificos
+
 @app.get('/get_item/{item_id}', response_model=ItemRead)
 async def get_item(item_id: int, db: Session = Depends(get_db)):
     item = db.query(ItemDB).filter(ItemDB.id == item_id).first()
+    
+    # Tratamento de exception's
     
     if item is None:
         raise HTTPException(status_code=404, detail='Item Not Found')
     
     return item
+
+# GET para todos os items da lista
+
+@app.get('/get_all_items', response_model=List[ItemRead])
+async def get_all_items(db: Session = Depends(get_db)):
+    items = db.query(ItemDB).all()
+    
+    return items
